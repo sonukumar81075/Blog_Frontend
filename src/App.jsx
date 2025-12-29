a0c9f07491ea46c233e625633edbf0ce0e1df1f1
sonu kumar saini <167969764+sonukumar81075@users.noreply.github.com>
2025-12-29 15:36:32 +0530

    Update API_URL to production endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/posts'
+const API_URL =  'https://blog-backend-2-nmng.onrender.com//api/posts'
 
 const initialForm = {
   title: '',
